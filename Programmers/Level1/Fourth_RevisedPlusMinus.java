5974bd5f8352c24d9a64798820bca97c8e5e6d98
[haileymoon] <moongirl1148@naver.com>
2022-01-08 18:44:59 +0900

    practice

---
+++ Programmers/Level1/Fourth_RevisedPlusMinus.java
@@ -0,0 +1,10 @@
+
+public class RevisedPlusMinus {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int count = 0; count<signs.length; count++){
+            answer = absolutes[count] * (signs[count] ? 1:-1);
+        }
+        return answer;
+    }
+}
