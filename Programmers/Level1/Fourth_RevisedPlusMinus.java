471e98a1a13fc68d4dded0ab730fb9379fa3f363
[haileymoon] <moongirl1148@naver.com>
2022-01-11 17:19:46 +0900

    잔디 심어지는지 체크

--- Programmers/Level1/Fourth_RevisedPlusMinus.java
+++ Programmers/Level1/Fourth_RevisedPlusMinus.java
@@ -1,5 +1,5 @@
 
-public class RevisedPlusMinus {
+public class Fourth_RevisedPlusMinus {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         for (int count = 0; count<signs.length; count++){
