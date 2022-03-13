9f8bf1d337e8795ef3d0884f689ff24e33acadd4
[haileymoon] <moongirl1148@naver.com>
2022-03-14 03:22:20 +0900

    백준연습문제

--- 백준문제/실버/달팽이_1913.py
+++ 백준문제/실버/달팽이_1913.py
@@ -1,59 +1,65 @@
-from collections import deque
-
+# wow this is legend 나 아까 왤케 어렵게 풀었냐;;
+# 패턴을 잘 보면 올라갈때랑 내려갈때만 횟수가 추가 된다는 걸 알수있음. 좌우로 움직일때는 횟수가 그대로임
+# bfs할 필요도 없음
 n = int(input())
 target = int(input())
-graph = [[-1] * n for i in range(n)]
-start = n//2
-graph[start][start] = 1
-queue = deque()
-queue.append((start, start))
-turn = -1
-count = 0
 value = 1
-line_length = 1
-dx = [-1,0,1,0]
-dy = [0,1,0,-1]
-target_pos = []
+start = n//2
+graph = [[-1]*n for _ in range(n)]
+limit = 1
+x,y= start, start
+graph[x][y] = 1
+while True:
+    # 한 방향에서 해당 길이 만큼 움직인다, 움직이고 turn하는데, 좌우로 turn할때는 횟수가 늘어나지 않는다.
+    # 함정은 위방향을 향했을때 limit이 1이 되기 위해 좌부터 for문을 시작한다.
+    for _ in range(limit):
+        # 위로
+        value = graph[x][y]
+        # 올라갔을때만 break 체크하면 됨
+        x -= 1
+        y += 0
+        graph[x][y] = value + 1
+        if x == 0 and y == 0:
+             break
+    if x == 0 and y == 0:
+        break
+    # if value-1 == n*n:
+    #     break
 
-# 같은 개수로 두 번씩 진행됨. 한칸 두번, 두칸 두번, 세칸 두번, 네칸 두번
-# turn은 매번 달라지지만 count는 두번 마다 바뀜
-while queue:
-    x,y = queue.popleft()
-    if -1 < x < n and -1 < y < n:
-        turn += 1
-        if turn == 4:
-            turn = 0
-        next_x = x
-        next_y = y
-    else: continue
+    for _ in range(limit):
+        # 오른쪽
+        value = graph[x][y]
+        x += 0
+        y += 1
+        graph[x][y] = value + 1
+    limit += 1
 
-    #범위 체크
-    for _ in range(2):
-        while line_length > count:
-            next_x += dx[turn]
-            next_y += dy[turn]
-            if -1 < next_x < n and -1 < next_y < n:
-                if graph[next_x][next_y] == -1:
-                    graph[next_x][next_y] = value + 1
-                    value = graph[next_x][next_y]
-                    count += 1
+    for _ in range(limit):
+        #아래
+        value = graph[x][y]
+        x += 1
+        y += 0
+        graph[x][y] = value + 1
 
-            else:
-                break
-        turn += 1
-        if turn == 4:
-            turn = 0
-        count = 0
-    line_length += 1
-    queue.append((next_x, next_y))
-    turn -= 1
+    for _ in range(limit):
+        # 왼쪽
+        value = graph[x][y]
+        x += 0
+        y -= 1
+        graph[x][y] = value + 1
+    limit += 1
 
+answer = []
 for i in range(n):
     for j in range(n):
         print(graph[i][j], end = " ")
         if graph[i][j] == target:
-            target_pos.append(i+1)
-            target_pos.append(j+1)
+            answer.append(i+1)
+            answer.append(j+1)
     print()
-for pos in target_pos:
+for pos in answer:
     print(pos, end = " ")
+
+
+
+
