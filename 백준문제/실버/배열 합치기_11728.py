8579e9da3357f88dd0315e25c38bf3f0f82fac17
[haileymoon] <moongirl1148@naver.com>
2022-03-17 06:38:16 +0900

    백준연습문제

--- 백준문제/실버/배열 합치기_11728.py
+++ 백준문제/실버/배열 합치기_11728.py
@@ -1,45 +1,20 @@
 n, m = map(int, input().split())
-a = list(input().split())
-b = list(input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
 # 작은 배열부터 정렬끝내고 나머지 쭉 출력하면 됨
 
-def combine(small, big):
-    pointer_big = 0
-    pointer_small = 0
-    count = len(big)+len(small)
-
-    while count != 0:
-        if pointer_big == len(big):
-            while pointer_small != len(small):
-                print(small[pointer_small], end = " ")
-                pointer_small += 1
-                count -= 0
-            break
-        if pointer_small == len(small):
-            while pointer_big != len(big):
-                print(big[pointer_big], end = " ")
-                pointer_big += 1
-                count -= 0
-            break
-
-        if big[pointer_big] != small[pointer_small]:
-            if big[pointer_big] > small[pointer_small]:
-                print(small[pointer_small], end = " ")
-                pointer_small +=1
-            else:
-                print(big[pointer_big], end = " ")
-                pointer_big +=1
-        else: # 같다면
-            print(big[pointer_big], end = " ")
-            pointer_small += 1
-            pointer_big += 1
-
-        count -= 1
-
-
-
-
-if n<m:
-    combine(a,b)
-else:
-    combine(b,a)
+pointer_a = 0
+pointer_b = 0
+while pointer_a < n and pointer_b < m:
+    if a[pointer_a] < b[pointer_b]:
+        print(a[pointer_a], end = " ")
+        pointer_a += 1
+    else:
+        print(b[pointer_b], end = " ")
+        pointer_b += 1
+while pointer_a < n:
+    print(a[pointer_a], end = " ")
+    pointer_a += 1
+while pointer_b < m:
+    print(b[pointer_b], end = " ")
+    pointer_b += 1
