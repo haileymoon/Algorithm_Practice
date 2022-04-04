b304230d842137dc933aa35d77aae620957a7b86
[haileymoon] <moongirl1148@naver.com>
2022-04-04 21:11:54 +0900

    leetcode연습문제

---
+++ Leet code/easy/MostCommonWord_819.py
@@ -0,0 +1,29 @@
+import re
+from collections import Counter
+
+
+def mostCommonWord(paragraph: str, banned) -> str:
+    """
+    :type paragraph: str
+    :type banned: List[str]
+    :rtype: str
+    """
+    # switch to lowercase
+    paragraph = paragraph.lower()
+    # remove punctuations and convert to list
+    paragraph = re.sub("[^a-zA-Z and ^" "]", " ", paragraph)
+    # split by words and convert to list
+    paragraph = list(paragraph.split())
+    # count by frequency
+    counter = list(Counter(paragraph).most_common())
+    print(counter)
+    # remove banned
+    for word in counter:
+        if word[0] in banned:
+            continue
+        else:
+            return word[0]
+
+par = "a, a, a, a, b,b,b,c, c"
+ban = ["a"]
+print(mostCommonWord(par, ban))
